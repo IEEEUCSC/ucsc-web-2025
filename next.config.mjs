926c8dc4ee1c12cb7d7475c8ec17eb8ba59184f3
Hashen Udara <106765093+HashenUdara@users.noreply.github.com>
2025-05-13 11:54:43 +0530

    config github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "images.unsplash.com",
-          pathname: "/**",
-        },
-        {
-            protocol: "https",
-            hostname: "cdn.pixabay.com",
-            pathname: "/**",
-          },
-      ],
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.pixabay.com",
+        pathname: "/**",
+      },
+    ],
+  },
+  assetPrefix: "",
+  basePath: "",
+  output: "export",
+};
+
+export default nextConfig;
